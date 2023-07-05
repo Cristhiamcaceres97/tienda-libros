da22cb58340c0d688499769364fbbcd6d3cff8aa
Cristhiamcaceres97 <133033840+Cristhiamcaceres97@users.noreply.github.com>
2023-07-05 15:29:07 -0600

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Inicio from "./Components/Inicio";
-import Carousel from "./Components/Carousel";
-import Buscador from "./Components/Buscador";
-import BookList from "./Components/BookList";
-import PaymentMethods from "./Components/PaymentMethods";
-import ListComponent from "./Components/ListComponent"; 
 
 const App = () => {
   return (
@@ -14,11 +9,6 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Inicio />} />
-        <Route path="/carousel" element={<Carousel />} />
-        <Route path="/buscador" element={<Buscador />} />
-        <Route path="/booklist" element={<BookList />} />
-        <Route path="/paymentmethods" element={<PaymentMethods />} />
-        <Route path="/ListComponent" element={<ListComponent />} />
       </Routes>
     </Router>
   );
